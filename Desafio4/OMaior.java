a1d60861cb0660b5ba607c7a7acc63b8b2380575
Ludmilla Perucci <85947891+LudmillaPerucci@users.noreply.github.com>
2022-06-10 12:35:35 -0300

    OMaior

--- Desafio4/OMaior.java
+++ Desafio4/OMaior.java
@@ -5,16 +5,13 @@ import java.util.*;
 public class OMaior {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int maiorAb;
+        int maiorAb, total;
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
-        
-        maiorAb = (a + b  + Math.abs(a - b)) / 2;
 
-        System.out.println("Eh o maior:" + maiorAb);
-        
-
-
-}
+        maiorAb = (a + b + Math.abs(a - b)) / 2;
+        total = (maiorAb + c + Math.abs(maiorAb - c)) / 2;
+        System.out.println(total + " eh o maior");
+    }
 }
